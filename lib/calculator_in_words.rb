48f6a7025a01730794f8d38c70edaa3a09934e82
Andrew M Westling & Catherine Anh Tran <student@epicodus.com>
2014-07-28 17:05:36 -0700

    doesn't work but it's 5pm

--- lib/calculator_in_words.rb
+++ lib/calculator_in_words.rb
@@ -1,11 +1,26 @@
 def calculator_in_words string
   string.downcase!
+
+
+
   string.sub!(/(what is)/, "")
+  string.sub!(/add/, "")
   string.sub!(/(plus)/, "+")
+  string.sub!(/and/, "+")
+  string.sub!(/minus/, "-")
+  string.sub!(/times/, "*")
+  string.sub!(/divided by/, "/")
   string.sub!(/(\?)/, "")
-  string.sub!(/\s/, '')
-  string
+  string.sub!(/\s/, "")
+  # string.gsub!(/\d/, "")
+
+  # array = string.scan(/(\+|\-|\/|\*|\d)/)
+  # print array
+
+
+
   eval string
+
 end
 
-puts calculator_in_words ("What is 4 plus 6?")
+puts calculator_in_words ("What is 24 divided by 5?")
--- spec/calculator_in_words_spec.rb
+++ spec/calculator_in_words_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('calculator_in_words')
 
 describe("calculator_in_words") do
-  it("should return calculated output for addition sentences") do
+  it("returns calculated output for addition/subtraction/division/multiplication sentences") do
     calculator_in_words("What is 4 plus 6?").should(eq(10))
   end
 end
